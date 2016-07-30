6b6121dce81898a2df2ace35250715e09862a7d5
yoshioka440 <yoshioka440@gmail.com>
2016-07-31 00:43:03 +0900

    ゲージの追加

--- Assets/Team_1/Scripts/ScrollManager.cs
+++ Assets/Team_1/Scripts/ScrollManager.cs
@@ -15,21 +15,13 @@ public class ScrollManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		distancePlayerToCamera = mainCamera.transform.position.y - player.transform.position.y;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 cameraPosition = new Vector3 (0, player.transform.position.y + distancePlayerToCamera, -10);
-
 		mainCamera.transform.position = cameraPosition;
 		backGround.transform.position = cameraPosition;
-
-//		Vector3 cameraPosition = new Vector3 (0, player.transform.position.y + 1, 0);
-//		mainCamera.transform.position = cameraPosition;
-//		backGround.transform.position = cameraPosition;
-
-//		backGround.transform.position = new Vector3 (0, player.transform.position.y + 1, 0);
 	}
 
 }
