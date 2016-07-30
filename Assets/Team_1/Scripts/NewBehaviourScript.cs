f9f0e23c50e851dc2321089c8c45b9c325456bdd
yoshioka440 <yoshioka440@gmail.com>
2016-07-30 20:04:30 +0900

    プロトタイプ実装

--- Assets/Team_1/Scripts/NewBehaviourScript.cs
+++ Assets/Team_1/Scripts/NewBehaviourScript.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
+	 
+	float currentDegree;
+	float elapsedTime;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		elapsedTime += Time.deltaTime;
+		currentDegree = this.transform.rotation.z + elapsedTime;
+
+		this.transform.Rotate(0, 0, currentDegree);
+		Debug.Log ("Time: " + Time.deltaTime);
+		Debug.Log ("elapsedTime: " + elapsedTime + ", Time: " + Time.deltaTime);
 	}
 }
