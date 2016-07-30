12e1f9bad72978d71b31c9e9f6f592297cb3b89e
Takamichi Anzai <zeal15300180@c-league.jp>
2016-07-31 07:06:42 +0900

    706 Playerまだですすいません

---
+++ Assets/Anzai/Scripts/StageScroll.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageScroll : MonoBehaviour {
+
+    int count;
+    public int endNum;
+    public GameObject player;
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        count = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((int)player.transform.position.y / 10 == count)
+        {
+            var Parent = GameObject.Find("StageGroup").transform;
+            int num = Parent.GetChildCount();
+            var Obj = Instantiate(prefab, Parent.transform.GetChild(num - 1).
+                transform.position + new Vector3(0, 11.0f, 0) ,
+                Quaternion.identity) as GameObject;
+            count++;
+            Obj.transform.parent = Parent;
+        }
+    }
+}
