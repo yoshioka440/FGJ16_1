12968283cbd179a4c701b88f4ca8098397be50b3
yoshioka440 <yoshioka440@gmail.com>
2016-07-31 07:47:38 +0900

    UI追加

---
+++ Assets/Team_1/Scripts/TitleManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene (1);
+//			var sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene ();
+//			if (sceneName == "Title") {
+//				
+//			}
+		}
+	}
+}
