75528dd185c1d5921deccfafb4159395d33a4a3d
yoshioka440 <yoshioka440@gmail.com>
2016-07-31 00:48:43 +0900

    Revert "プロトタイプの実装"
    This reverts commit 9cd69ab37a443e076e8990f43239cde4ab77124b.

--- Assets/Team_1/Scripts/NewBehaviourScript.cs
+++ Assets/Team_1/Scripts/NewBehaviourScript.cs
@@ -2,22 +2,14 @@
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
-	 
-	float currentDegree;
-	float elapsedTime;
 
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		elapsedTime += Time.deltaTime;
-		currentDegree = this.transform.rotation.z + elapsedTime;
-
-		this.transform.Rotate(0, 0, currentDegree);
-		Debug.Log ("Time: " + Time.deltaTime);
-		Debug.Log ("elapsedTime: " + elapsedTime + ", Time: " + Time.deltaTime);
+	void Update () {
+	
 	}
 }
