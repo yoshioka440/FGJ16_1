7807b366b5e4dbf0433a878ccc2a4b372f386e91
yoshioka440 <yoshioka440@gmail.com>
2016-07-31 16:24:04 +0900

    修正

---
+++ Assets/Team_1/Scripts/BirdController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdController : MonoBehaviour {
+
+	public GameObject bird;
+	public Transform point1;
+	public Transform point2;
+	public GameObject unko;
+	bool wentTo2From1;
+	float jumpTime;
+	public float flyingTime = 5.0f;
+	bool to2 = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (to2) {
+			jumpTime += Time.deltaTime;
+			bird.transform.position = Vector3.Lerp (point1.transform.position, point2.transform.position, jumpTime / flyingTime);
+			if (jumpTime / flyingTime >= 1.0f) {
+				to2 = false;
+				jumpTime = 0.0f;
+				bird.transform.Rotate (new Vector3(0, 180, 0));
+			}
+		} else {
+			jumpTime += Time.deltaTime;
+			bird.transform.position = Vector3.Lerp (point2.transform.position, point1.transform.position, jumpTime / flyingTime);
+			if (jumpTime / flyingTime >= 1.0f) {
+				to2 = true;
+				jumpTime = 0.0f;
+				bird.transform.Rotate (new Vector3(0, 180, 0));
+			}
+		}
+	}
+}
---
+++ Assets/Team_1/Sprites/UnkoController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnkoController : MonoBehaviour {
+
+	float time;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time > 5.0f) {
+			Destroy (this.gameObject);
+		}
+	}
+
+//	void OnCollisionEnter2D (Collision2D collision) {
+//		if (collision.transform.tag == "Finish") {
+//			UnityEngine.SceneManagement.SceneManager.LoadScene ();
+//		}
+//	}
+}
